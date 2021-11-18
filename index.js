07116c1445214cb4e58082c5e9b2d09a0cae1d96
Kevin Rogers <kevinarogers1@gmail.com>
2021-11-17 19:17:59 -0500

    commit tree 1c34df96c12951befff4cc81dbf626be2e587940

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback) {
+    console.log('callback function activated');
+    callback(); 
+}
+
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return () => console.log('Hello');
+}
